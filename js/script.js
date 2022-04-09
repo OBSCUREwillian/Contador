fab090eccf41537e94019264d3c6e754ceba0448
Will <willian_aud521@hotmail.com>
2022-04-09 15:14:25 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// Elements
+const display = document.getElementById('display');
+const subtractBtn = document.getElementById('subtract');
+const addBtn = document.getElementById('add');
+const arrowUp = document.getElementById('arrow-up');
+const arrowDown = document.getElementById('arrow-down');
+
+
+// Variables
+var currentNumber = 0;
+
+
+
+// Main
+addBtn.addEventListener("click", incremment);
+subtractBtn.addEventListener("click", decremment);
+
+
+
+// Functions
+function incremment(){
+  display.innerText = ++currentNumber;
+  checkDisplayLimit()
+}
+
+
+function decremment(){
+  display.innerText = --currentNumber;
+  checkDisplayLimit()
+}
+
+
+function checkDisplayLimit(){
+
+  // Disable button toggle
+  addBtn.disabled = display.innerText == 10;
+  subtractBtn.disabled = display.innerText == -10;
+
+
+  // Class arrows toggle
+  if(addBtn.disabled)
+  arrowUp.classList.add('disabled');
+  else
+  arrowUp.classList.remove('disabled');
+
+  if(subtractBtn.disabled)
+  arrowDown.classList.add('disabled');
+  else
+  arrowDown.classList.remove('disabled');
+
+
+  // Add red color in negative numbers
+  display.innerText < 0 ? display.style.color = "red" : display.style.color = '#fff';
+
+}
